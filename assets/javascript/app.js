59e5e030f65f2a5b54ed0e7c8d48ebcc06887469
CGibeau <claire.gibeau@outlook.com>
2019-03-06 21:55:47 -0800

    wip

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,44 @@
 //JavaScript
+
+//create start button for timer
+
+//global variables
+var clockRunning = false;
+var timer;
+var duration = 120;
+
+//click event to call start
+window.onload = function () {
+    $("#start").on("click", start);
+}
+
+//start timer
+function start() {
+$("#start").hide();
+$("#questions").show();
+$("#trackTime").show();
+
+    if (!clockRunning) {
+        timer = setInterval(onInterval, 1000);
+        clockRunning = true;
+    }
+
+
+
+
+}
+
+
+
+function onInterval() {
+    duration--;
+    $("#counterDisplay").text(duration);
+
+    if (duration === 0) {
+        clearInterval(timer);
+        clockRunning = false;
+    }
+}
+
+//create way to track answers and show results 1-correct answers, 2-incorrect answers, 3-unanswered
+
