a7e60aa034b04a79d317e3aa0f72c61f731ebb01
CGibeau <claire.gibeau@outlook.com>
2019-03-07 19:54:12 -0800

    wip

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TriviaGame
+
+
+Seattle Fun Fact Source: https://travelingwiththejones.com/2014/09/03/42-fun-facts-about-seattle-washington/
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,17 +14,14 @@ window.onload = function () {
 
 //start timer
 function start() {
-$("#start").hide();
-$("#questions").show();
-$("#trackTime").show();
+    $("#start").hide();
+    $("#questions").show();
+    $("#trackTime").show();
 
     if (!clockRunning) {
         timer = setInterval(onInterval, 1000);
         clockRunning = true;
     }
-
-
-
 
 }
 
@@ -37,8 +34,59 @@ function onInterval() {
     if (duration === 0) {
         clearInterval(timer);
         clockRunning = false;
+        checkAnswers();
     }
 }
 
+
+
+
 //create way to track answers and show results 1-correct answers, 2-incorrect answers, 3-unanswered
 
+//how many answered correctly?
+
+
+
+//how many answered incorrectly?
+
+//how many unanswered?
+
+
+function checkAnswers() {
+    var correctAnswer = 0;
+    var incorrectAnswer = 0;
+    var noAnswer = 0;
+    var arr = $(".triviaQuestions");
+    for (var i = 0; i < arr.length; i++) {
+        var questionDiv = $(arr[i]);
+        var answer = questionDiv.find("input").filter(":checked");
+        if (answer.length != 1) {
+            noAnswer++;
+    
+        }
+        else {
+            var isCorrect = $(answer[0]).data("answer");
+            if (isCorrect === true) {
+                correctAnswer++;
+            }
+            else {
+                incorrectAnswer++;
+            
+            }
+
+        }
+
+    }
+
+    $("#correctAnswers").text(correctAnswer);
+    $("#incorrectAnswers").text(incorrectAnswer);
+    $("#noAnswers").text(noAnswer);
+
+
+    $("#start").hide();
+    $("#questions").hide();
+    $("#trackTime").hide();
+
+$("#score").show();
+
+}
